75ae3522a296b6cead857d0f5c1ab345f0424e8c
Софья <@WINSERVER>
2025-03-16 22:42:48 +0300

    Добавила кнопки с разными путями и отражение их на второй странице

--- project/server.py
+++ project/server.py
@@ -19,15 +19,39 @@ class RequestHandler(BaseHTTPRequestHandler):
             post_data = self.rfile.read(content_length)
             request_data = json.loads(post_data.decode("utf-8"))
 
+            departure = request_data.get("departure", "Москва")
+            arrival = request_data.get("arrival", "Волгоград")
+
             response_data = {
-                "departure": request_data["departure"],
-                "arrival": request_data["arrival"],
+                "departure": departure,
+                "arrival": arrival,
                 "totalTime": "1 день 23 часа",
                 "totalPrice": "20000",
-                "steps": [
-                    {"departureTime": "01:59", "departureStation": "Павелецкий вокзал", "departureCity": "Москва", "transport": "Поезд", "arrivalTime": "11:22", "arrivalStation": "Придача", "arrivalCity": "Воронеж", "transferDuration": 13},
-                    {"departureTime": "00:53", "departureStation": "Придача", "departureCity": "Воронеж", "transport": "Поезд", "arrivalTime": "12:28", "arrivalStation": "Ростов Главный", "arrivalCity": "Ростов-на-Дону", "transferDuration": 2.5},
-                    {"departureTime": "15:00", "departureStation": "Ростов Главный", "departureCity": "Ростов-на-Дону", "transport": "Поезд", "arrivalTime": "01:46", "arrivalStation": "Волгоград-1", "arrivalCity": "Волгоград"}
+                "direct": [  # Прямые маршруты
+                    {"time": "08:00", "duration": "10 ч", "price": 5000},
+                    {"time": "14:30", "duration": "11 ч", "price": 5200}
+                ],
+                "with_rest": [  # Маршруты с отдыхом
+                    {"time": "18:00", "duration": "15 ч", "price": 4800}
+                ],
+                "explore": [  # Маршруты через другие города
+                    {"time": "06:45", "duration": "20 ч", "price": 5500}
+                ],
+                "steps": [  # Детальная информация о маршруте
+                    {
+                        "departureTime": "01:59", "departureStation": "Павелецкий вокзал", "departureCity": "Москва",
+                        "transport": "Поезд", "arrivalTime": "11:22", "arrivalStation": "Придача", "arrivalCity": "Воронеж",
+                        "transferDuration": 13
+                    },
+                    {
+                        "departureTime": "00:53", "departureStation": "Придача", "departureCity": "Воронеж",
+                        "transport": "Поезд", "arrivalTime": "12:28", "arrivalStation": "Ростов Главный", "arrivalCity": "Ростов-на-Дону",
+                        "transferDuration": 2.5
+                    },
+                    {
+                        "departureTime": "15:00", "departureStation": "Ростов Главный", "departureCity": "Ростов-на-Дону",
+                        "transport": "Поезд", "arrivalTime": "01:46", "arrivalStation": "Волгоград-1", "arrivalCity": "Волгоград"
+                    }
                 ]
             }
 
